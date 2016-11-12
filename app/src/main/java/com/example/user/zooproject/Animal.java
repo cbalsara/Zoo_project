c90182dbdeba4f1004df6ac219268d690b10ba73
Cyrus <cdbalsara@icloud.com>
2016-11-12 12:40:06 +0000

    set up feline class that extends animal abstract

--- app/src/main/java/com/example/user/zooproject/Animal.java
+++ app/src/main/java/com/example/user/zooproject/Animal.java
@@ -4,11 +4,12 @@ package com.example.user.zooproject;
  * Created by user on 12/11/2016.
  */
 
-public class Animal {
-        private String name;
-        private int age;
-        private String type;
-        private boolean fed;
+public abstract class Animal {
+
+            private String name;
+            private int age;
+            private String type;
+            private boolean fed;
 
 
         public Animal(String name, int age, String type, boolean fed) {
---
+++ app/src/main/java/com/example/user/zooproject/Feline.java
@@ -0,0 +1,15 @@
+package com.example.user.zooproject;
+
+/**
+ * Created by user on 12/11/2016.
+ */
+
+public class Feline extends Animal {
+
+
+    public Feline(String name, int age, String type, boolean fed) {
+        super(name, age, type, fed);
+    }
+
+    
+}
