dac72e96ef62091804c122bc4255b687bc005203
Cyrus <cdbalsara@icloud.com>
2016-11-12 11:55:17 +0000

    added get name method for animal class

---
+++ app/src/main/java/com/example/user/zooproject/Animal.java
@@ -0,0 +1,27 @@
+package com.example.user.zooproject;
+
+/**
+ * Created by user on 12/11/2016.
+ */
+
+public class Animal {
+        private String name;
+        private int age;
+        private String type;
+        private boolean fed;
+
+
+        public Animal(String name, int age, String type, boolean fed) {
+            this.name = name;
+            this.age = age;
+            this.type = type;
+            this.fed = fed;
+        }
+
+        public String getName(){
+            return this.name;
+        }
+
+
+    }
+
