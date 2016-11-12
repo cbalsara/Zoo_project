0cba09e75ed468a5033c65e7ab0ca2383fa3aa56
Cyrus <cdbalsara@icloud.com>
2016-11-12 15:47:54 +0000

    created Tiger class and impleement what it inherits from abstract class feline as well as what new features the class contains

---
+++ app/src/main/java/com/example/user/zooproject/Tiger.java
@@ -0,0 +1,29 @@
+package com.example.user.zooproject;
+
+/**
+ * Created by user on 12/11/2016.
+ */
+
+public class Tiger extends Feline {
+
+    private String name;
+    private int age;
+    private boolean fed;
+
+    public Tiger(String type, String specie, String name, int age, boolean fed){
+
+        super(type, specie);
+
+        this.name = name;
+        this.age = age;
+        this.fed = fed;
+
+    }
+
+
+
+
+
+
+
+}
