149eb0a857391adcf12a86fecc495a2ea4b757ff
Cyrus <cdbalsara@icloud.com>
2016-11-13 11:35:15 +0000

    created owl class that extends from bird abstract class

---
+++ app/src/main/java/com/example/user/zooproject/Owl.java
@@ -0,0 +1,25 @@
+package com.example.user.zooproject;
+
+/**
+ * Created by user on 13/11/2016.
+ */
+
+public class Owl extends Bird {
+
+    private String name;
+    private int age;
+    private boolean fed;
+
+    public Owl(String type, String specie, int age, boolean fed){
+        super(type, specie);
+
+        
+
+    }
+
+
+
+
+
+
+}
