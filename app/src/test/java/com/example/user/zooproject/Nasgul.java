58ce5860f83500b8f7621ab8c7311481e94616bb
Cyrus <cdbalsara@icloud.com>
2016-11-13 14:04:27 +0000

    created nasgul class that extends mythical class

---
+++ app/src/test/java/com/example/user/zooproject/Nasgul.java
@@ -0,0 +1,24 @@
+package com.example.user.zooproject;
+
+/**
+ * Created by user on 13/11/2016.
+ */
+
+public class Nasgul extends Mythical {
+
+    private String name;
+    private int age;
+    private boolean fed;
+
+    public Nasgul(String type, String specie, String name, int age, boolean fed){
+
+        super(type,specie);
+
+        this.name = name;
+        this.age = age;
+        this.fed = fed;
+
+    }
+
+
+}
