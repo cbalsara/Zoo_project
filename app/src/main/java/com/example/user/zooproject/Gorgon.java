e00727bfd655e364494e4c938426ee3d00bbe577
Cyrus <cdbalsara@icloud.com>
2016-11-13 14:31:36 +0000

    created gorgon class that extends from mythical

---
+++ app/src/main/java/com/example/user/zooproject/Gorgon.java
@@ -0,0 +1,24 @@
+package com.example.user.zooproject;
+
+/**
+ * Created by user on 13/11/2016.
+ */
+
+public class Gorgon extends Mythical {
+
+    private String name;
+    private int age;
+    private boolean fed;
+
+    public Gorgon(String type, String specie, String name, int age, boolean fed){
+
+        super(type,specie);
+
+        this.name = name;
+        this.age = age;
+        this.fed = fed;
+
+    }
+
+    
+}
---
+++ app/src/test/java/com/example/user/zooproject/GorgonTest.java
@@ -0,0 +1,8 @@
+package com.example.user.zooproject;
+
+/**
+ * Created by user on 13/11/2016.
+ */
+
+public class GorgonTest {
+}
