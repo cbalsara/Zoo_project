d754e0a91fcd68d64376efada58e51a200fcc33c
Cyrus <cdbalsara@icloud.com>
2016-11-13 13:33:35 +0000

    created abstract  mythical class that extends abstract animal class

---
+++ app/src/main/java/com/example/user/zooproject/Mythical.java
@@ -0,0 +1,26 @@
+package com.example.user.zooproject;
+
+/**
+ * Created by user on 13/11/2016.
+ */
+
+abstract public class Mythical extends Animal {
+
+    private String type;
+    private String specie;
+
+    public Mythical(String type, String specie){
+        super(type, specie);
+
+        this.type = "Carnivore";
+        this.specie = "Myth";
+    }
+
+    
+
+
+
+
+
+
+}
