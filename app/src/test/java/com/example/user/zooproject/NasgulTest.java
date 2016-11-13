3278086a35208cd8d0b1adbabc1c697bb71a10bb
Cyrus <cdbalsara@icloud.com>
2016-11-13 14:07:58 +0000

    added nasgul has name test

---
+++ app/src/test/java/com/example/user/zooproject/NasgulTest.java
@@ -0,0 +1,29 @@
+package com.example.user.zooproject;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by user on 13/11/2016.
+ */
+
+public class NasgulTest {
+
+    Nasgul nasgul;
+
+    @Before
+    public void before(){
+        nasgul = new Nasgul("", "", "Bob", 10000, false);
+    }
+
+    @Test
+    public void testingHasName(){
+        assertEquals( "Bob", nasgul.hasName() );
+    }
+
+
+
+
+}
