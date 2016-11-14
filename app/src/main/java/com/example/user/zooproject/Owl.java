a921fd27504dd59e1ff9c0650d73391a53af6d6b
Cyrus <cdbalsara@icloud.com>
2016-11-14 15:03:37 +0000

    made the owl class implement the flyable interface

--- app/src/main/java/com/example/user/zooproject/Owl.java
+++ app/src/main/java/com/example/user/zooproject/Owl.java
@@ -4,7 +4,7 @@ package com.example.user.zooproject;
  * Created by user on 13/11/2016.
  */
 
-public class Owl extends Bird {
+public class Owl extends Bird implements Flyable {
 
     private String name;
     private int age;
@@ -30,6 +30,10 @@ public class Owl extends Bird {
 
     public boolean hasBeenFed(){
         return this.fed;
+    }
+
+    public String fly(){
+        return "flapping";
     }
 
 }
