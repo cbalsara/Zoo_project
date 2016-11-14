dd1cee35d1b2210fdb5b631ad6ec2fab25a1a77e
Cyrus <cdbalsara@icloud.com>
2016-11-14 15:02:47 +0000

    made the eagle class implelement the flyable interface

--- app/src/main/java/com/example/user/zooproject/Eagle.java
+++ app/src/main/java/com/example/user/zooproject/Eagle.java
@@ -4,7 +4,7 @@ package com.example.user.zooproject;
  * Created by user on 13/11/2016.
  */
 
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Flyable {
 
     private String name;
     private int age;
@@ -30,6 +30,10 @@ public class Eagle extends Bird {
 
     public boolean hasBeenFed(){
         return this.fed;
+    }
+
+    public String fly(){
+        return "flapping";
     }
 
 }
