f47793fefeb63c33c98db93c44c7e78dc1a5f2cd
Cyrus <cdbalsara@icloud.com>
2016-11-15 15:50:20 +0000

    added foodeaten, feed animal stomach full and got to toilet method for lion and gorgon

--- app/src/main/java/com/example/user/zooproject/Lion.java
+++ app/src/main/java/com/example/user/zooproject/Lion.java
@@ -8,16 +8,16 @@ public class Lion extends Feline implements Purrable {
 
     private String name;
     private int age;
-    private boolean fed;
+    private Food[] stomach;
 
 
-    public Lion(Enum type, String specie, String name, int age, boolean fed) {
+    public Lion(Enum type, String specie, String name, int age) {
 
         super(type, specie);
 
         this.name = name;
         this.age = age;
-        this.fed = fed;
+        this.stomach = new Food [25];
     }
 
     public String hasName(){
@@ -28,8 +28,33 @@ public class Lion extends Feline implements Purrable {
         return this.age;
     }
 
-    public boolean hasBeenFed(){
-        return this.fed;
+    //check to see how much food consumed
+    public int foodEaten(){
+        int counter = 0;
+        for(int Food = 0; Food < stomach.length; Food++){
+            if (stomach[Food] != null) counter++;
+        }
+        return counter;
+    }
+    //actually feed the animal
+    public void FeedAnimal(Food food){
+        if( stomachFull() ){
+            return;
+        }
+        int fillFood = foodEaten();
+        stomach[fillFood] = food;
+    }
+
+    //    Make sure that the animal can not eat excess food
+    public boolean stomachFull(){
+        return foodEaten() == stomach.length;
+    }
+
+    //    make sure that the animals stomach can be emptied
+    public void goToToilet(){
+        for (int food = 0; food < stomach.length; food++){
+            stomach[food] = null;
+        }
     }
 
     public String purr(){
